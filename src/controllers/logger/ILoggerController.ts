9987a51ce5974d2b0a095cdf914b30ca886fd7c6
cthacker-udel <cthacker@udel.edu>
2023-04-01 02:01:06 -0400

    [master] Added logger controller interface + class

--- src/controllers/logger/ILoggerController.ts
+++ src/controllers/logger/ILoggerController.ts
@@ -1,1 +1,11 @@
-export interface ILoggerController {}
+/* eslint-disable @typescript-eslint/no-extraneous-class -- disabled */
+
+import { ExceptionLog } from "@/models/logger/ExceptionLog";
+import { MongoService } from "@/services/mongo/MongoService";
+
+export abstract class ILoggerController {
+    static LogException: (
+        mongoService: MongoService,
+        exceptionLog: ExceptionLog,
+    ) => Promise<boolean>;
+}
